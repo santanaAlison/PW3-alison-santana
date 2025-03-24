eb5fe2b6f19689713972cf4681aac6ba75eb51e8
alison santana <alison.santana2@etec.sp.gov.br>
2025-03-24 19:48:11 -0300

    aula com professor joão

--- aula03/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/usuarios','App\Http\Controllers\UsuariosController@index');
-Route::post('/usuarios','App\Http\Controllers\UsuariosController@store');
-Route::delete('/usuarios','App\Http\Controllers\UsuariosController@destroy');
